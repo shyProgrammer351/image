68ed9262c636391ed557dd217a3a7be062bbbb37
shyProgrammer351 <123141646+shyProgrammer351@users.noreply.github.com>
2023-01-20 13:36:19 +0900

    Add files via upload

---
+++ ChatGUI.java
@@ -0,0 +1,25 @@
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import chat.ChatPanel;
+
+/**************
+ * 채팅 프로그램의 GUI 버전
+ * @author So Young Park
+ * @version 0.8
+ */	
+public class ChatGUI
+{
+	public static void main(String[] args) 
+	{
+		ChatPanel panel = new ChatPanel();
+
+		JFrame frame = new JFrame( "채팅" );
+		frame.getContentPane().add( panel );
+		frame.setPreferredSize( new Dimension( 320, 445 ) );	
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		frame.pack();
+		frame.setVisible( true );
+	}
+}
+
+
---
+++ ThreeCupGameGUI.java
@@ -0,0 +1,23 @@
+import javax.swing.JFrame;
+import threeCupGame.ThreeCupGamePanel;
+
+/**************
+ * 구슬찾기 게임의 GUI 버전
+ * @author So Young Park
+ * @version 0.8
+ */	
+public class ThreeCupGameGUI
+{
+	public static void main(String[] args) 
+	{
+		ThreeCupGamePanel panel = new ThreeCupGamePanel();
+
+		JFrame frame = new JFrame( "구슬찾기" );
+		frame.getContentPane().add( panel );
+		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		frame.pack();
+		frame.setVisible( true );
+	}
+}
+
+
